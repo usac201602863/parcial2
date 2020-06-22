188d9aa6bb73c938d049a96f92e069de4328b3dc
usac201612200 <3555026900110@ingenieria.usac.edu.gt>
2020-06-22 01:42:12 -0600

    Se agrego la parte del chat

--- cliente/201602863/cliente.py
+++ cliente/201602863/cliente.py
@@ -44,6 +44,24 @@ def reproducir(audio="archivo",d=1):    # Funcion para reproducir audio
     logging.info('Recepcion finalizada. Iniciando reproduccion')
     os.system('aplay '+audio+'.wav')
     logging.info('El mensaje se ha terminado de reproducir.')
+
+def leer_archivo(archivo):                          #WAIG Funcion para leer el archivo y mostrar opciones
+    datos = open(archivo, "r")                      #WAIG Abrimos el archivo
+    numero = 1                                      #WAIG Variable que nos ayudara a mostrar el numero de elemento            
+    for linea in datos:                             #WAIG For que leera linea por linea los elementos del archivo
+        print(str(numero)+ ". " + linea)            #WAIG Se muestra en pantalla cada elemento
+        numero += 1                                 #WAIG Se le suma un numero por cada elemento mostrado
+    datos.close()                                   #WAIG Se cierra el documento
+
+def selec_usu_grup(archivo,numero):                 #WAIG Funcion para seleccionar opcion de la lista
+    datos = open(archivo, "r")                      #WAIG Abrimos el archivo
+    list_datos = []                                 #WAIG Se declara una lista para aguardar los elementos de la lista
+    for linea in datos:                             #WAIG For para leer cada elemento de la lista
+        list_datos.append(linea)                    #WAIG Agrega un elemento a la lista
+    datos.close()                                   #WAIG Se cierra el documento
+    return str(list_datos[numero - 1])              #WAIG Se escoge el numero de elemento de la lista 
+
+
 ##########################################################################################################
 #               Configuracion MQTT
 logging.basicConfig(    # Configuracion inicial para logging.
@@ -55,10 +73,14 @@ def on_connect(client, userdata, flags, rc):    #Handler en caso suceda la conex
     connectionText = "CONNACK recibido del broker con codigo: " + str(rc)
     logging.info(connectionText)
 
-
 def on_publish(client, userdata, mid):          #Handler en caso se publique satisfactoriamente
     publishText = "Publicacion satisfactoria."
-    logging.debug(publishText)
+    #logging.debug(publishText)
+
+def on_message(client, userdata, msg):
+    menm = str(msg.payload)
+    topic_recibido = 'Mensaje entrante del topic ' + str(msg.topic) + ': ' +  menm.replace("b","")
+    logging.info(topic_recibido)
 
 logging.info("Conexion exitosa cliente MQTT") #Mensaje en consola
 
@@ -66,6 +88,7 @@ logging.info("Conexion exitosa cliente MQTT") #Mensaje en consola
 client = paho.Client(clean_session=True) #Nueva instancia de cliente
 client.on_connect = on_connect #Se configura la funcion "Handler" cuando suceda la conexion
 client.on_publish = on_publish #Se configura la funcion "Handler" que se activa al publicar algo
+client.on_message = on_message #WAIG Se configura la funcion "Handler" que se activa al llegar un mensaje a un topic subscrito
 client.username_pw_set(MQTT_USER, MQTT_PASS) #Credenciales requeridas por el broker
 client.connect(host=MQTT_HOST, port = MQTT_PORT) #Conectar al servidor remoto
 """
@@ -96,8 +119,13 @@ def sendFTR(command,Filsize, retain = False):  # Funcion para publicar
 
 ###################################################################################################3
 #               HILO PRINCIPAL
-
+DEFAULT_DELAY_2 = 0.5                                 #WAIG Tiempo 0.5 s
+User_ID = 201612200                                 #WAIG ID del cliente
 DEFAULT_DELAY = 30 #1 minuto
+
+#WAIG Subscripcion a los topic 
+client.subscribe(('usuarios/10/'+str(User_ID), 1))
+
 try:
 
     #print(usuario().encode("utf-8"))
@@ -118,12 +146,45 @@ try:
         if(opcion.isnumeric()):
             opcion=int(opcion)
             if(opcion==1):  # Enviar texto a usuario
-                publishData("test","Mensaje Inicial pr80")
-                logging.info("Los datos han sido enviados al broker") 
-                sendCommand(ALIVE)
-                sendAlive()
+                #publishData("test","Mensaje Inicial pr80")
+                #logging.info("Los datos han sido enviados al broker") 
+                print("¿A que usuario desea enviar el mensaje?:")           #WAIG Preguntamos a que usuario se quiere enviar el mensaje
+                leer_archivo("usuario.txt")                                 #WAIG leemos el archivo de usuario y lo despleguegamos
+                usuarioa = int(input('Ingrese el numero:'))
+                nusu = int(selec_usu_grup('usuario.txt',usuarioa))
+                topic = 'usuarios/'+str(Grupo)+'/'+ str(nusu)               #WAIG Se forma el topic para mandar el mensaje
+                client.loop_start()                                         #WAIG Conmenzamos a estar pendiente de mensajes entrantes
+                time.sleep(DEFAULT_DELAY_2)                                 #WAIG Esperamos 5 milisegundos para que aparezca el mensaje de la conexion al servidor
+                print('*************CHAT*****************')
+                print('Presione enter para enviar el mensaje: ')
+                while True:    
+                    mensaje = input("")                                        #WAIG Se obtiene el mensaje
+                    client.publish(topic,mensaje, qos = 1, retain = False)     #WAIG Se envia el mensaje a travez del topic
+                    if on_connect :                                            #WAIG Si le llega un mensaje 
+                        time.sleep(DEFAULT_DELAY_2)                            #WAIG Esperara 0.5 s para mostrar el mensaje,
+                        print('Presione enter para enviar el mensaje: ') #     esto es mas que todo para que se vea ordenado
+
+                    #sendCommand(ALIVE)
+                    #sendAlive()
+
             elif(opcion==2):    # Enviar texto a sala
-                pass
+                print('¿A que grupo desea enviar el mensaje?:')             #WAIG Preguntamos a que sala se quiere enviar el mensaje
+                leer_archivo('salas.txt')                                   #WAIG Leemos el archivo de salas y lo desplegamos 
+                salac = int(input('Ingrese el numero:'))
+                nsal = int(selec_usu_grup('salas.txt',salac))                     
+                topic = 'salas/'+str(Grupo)+'/'+ 'S' + str(nsal)            #WAIG Se forma el topic para mandar el mensaje
+                client.loop_start()                                         #WAIG Conmenzamos a estar pendiente de mensajes entrantes
+                time.sleep(DEFAULT_DELAY_2)                                   #WAIG Esperamos 5 milisegundos para que aparezca el mensaje de la conexion al servidor   
+                print('*************CHAT*****************')
+                print('Presione enter para enviar el mensaje: ')
+                while True:    
+                    mensaje = input("")                                         #WAIG Se obtiene el mensaje
+                    client.publish(topic,mensaje, qos = 1, retain = False)      #WAIG Se envia el mensaje a travez del topic
+                    if on_connect :                                             #WAIG Si le llega un mensaje 
+                        time.sleep(DEFAULT_DELAY_2)                               #WAIG Esperara 0.5 s para mostrar el mensaje,
+                        print('Presione enter para enviar el mensaje: ') #     esto es mas que todo para que se vea ordenado
+
+
             elif(opcion==3):    # Enviar voz a usuario
                 # Selecciona usuario a publicar
                 # Ingresa duracion del audio
